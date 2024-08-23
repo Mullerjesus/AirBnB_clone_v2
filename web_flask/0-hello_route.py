545175c3f9f1e85bb539df9983da52a151b6a205
Muluken <mulukeneshetu70@gmail.com>
2024-08-23 18:13:04 +0300

    AirBnB clone - Web framework

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,17 +1,30 @@
 #!/usr/bin/python3
+"""
+This module implements a simple Flask application that returns
+the greeting 'Hello HBNB!' when accessed on the root URL.
+"""
 
 from flask import Flask
 
 app = Flask(__name__)
 
-"""Defines the route"""
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """Return a simple greeting.
+    
+    This function is mapped to the root URL and returns
+    the string 'Hello HBNB!'.
+    """
+    return "Hello HBNB!"
 
-@app.route("/", strict_slashes=False)
-def hello_world():
-    """return a given string"""
-    return("hbnb")
+@app.route('/favicon.ico', methods=['GET'])
+def favicon():
+    """Handle the favicon request.
 
-if __name__ == '__main__':
-    """Starts the flask development server"""
+    This function returns a 204 No Content response when
+    a browser requests the favicon.ico.
+    """
+    return '', 204  # No Content
+
+if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
-
