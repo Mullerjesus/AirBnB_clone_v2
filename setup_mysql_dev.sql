ac13b2d346c614d2c66fca0cfc56cc654015c227
Muluken <mulukeneshetu70@gmail.com>
2024-08-24 12:54:44 +0300

    AirBnB clone - Web framework

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,25 +1,14 @@
--- This SQL script creates the necessary tables for the AirBnB clone project  
+-- Check if database hbnb_dev_db exists, if not, create it  
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;  
 
--- Create a new database  
-CREATE DATABASE IF NOT EXISTS airbnb_db;  
+-- Check if user hbnb_dev exists, if not, create it  
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';  
 
--- Use the newly created database  
-USE airbnb_db;  
+-- Grant all privileges on hbnb_dev_db to hbnb_dev user  
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';  
 
--- Create the states table  
-CREATE TABLE IF NOT EXISTS states (  
-    id INT AUTO_INCREMENT PRIMARY KEY,  
-    name VARCHAR(255) NOT NULL,  
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,  
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP  
-);  
+-- Grant SELECT privilege on performance_schema to hbnb_dev user  
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';  
 
--- Create the cities table  
-CREATE TABLE IF NOT EXISTS cities (  
-    id INT AUTO_INCREMENT PRIMARY KEY,  
-    state_id INT NOT NULL,  
-    name VARCHAR(255) NOT NULL,  
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,  
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,  
-    FOREIGN KEY (state_id) REFERENCES states(id) ON DELETE CASCADE  
-);
+-- Flush privileges to ensure that changes take effect  
+FLUSH PRIVILEGES;
