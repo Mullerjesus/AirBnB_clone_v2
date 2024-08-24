41557ffd4def6ba7454ba5f73c7797f31ae5866d
Muluken <mulukeneshetu70@gmail.com>
2024-08-24 12:11:10 +0300

    AirBnB clone - Web framework

---
+++ models/__init__.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3  
+# models/__init__.py  
+"""This package contains all the data models for the AirBnB clone project."""
--- models/city.py
+++ models/city.py
@@ -1,8 +1,21 @@
-#!/usr/bin/python3
-from models.base_model import BaseModel
+#!/usr/bin/python3  
+"""  
+This module defines the City class which represents a city entity  
+in the AirBnB clone project.  
+"""  
+from models.base_model import BaseModel  
 
-class City(BaseModel):
-    """City class that represents a city in the AirBnB clone."""
+class City(BaseModel):  
+    """  
+    The City class represents a city in the AirBnB clone.  
+    It inherits from BaseModel.  
+    """  
+    
+    def __init__(self, state_id, name):  
+        """Initializes a new City instance."""  
+        self.state_id = state_id  # ID of the State to which this city belongs  
+        self.name = name  # The name of the city  
 
-    state_id = ""
-    name = ""
+    def __str__(self):  
+        """Returns a string representation of the City instance."""  
+        return f"[City] ({self.id}) {self.__dict__}"
