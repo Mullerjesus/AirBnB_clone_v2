fc554ec110e00c2b7e899988d2dd4456cc64aa02
Muluken <mulukeneshetu70@gmail.com>
2024-08-24 17:02:35 +0300

    AirBnB_clone_v2

---
+++ models/base_model.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+Module base_model
+Defines the BaseModel class for the project.
+"""
+
+class BaseModel:
+    """
+    BaseModel class that defines all common attributes/methods for other classes.
+    """
+
+    def __init__(self, *args, **kwargs):
+        """
+        Initialize the BaseModel instance.
+        """
+        # Initialization code here
+        pass
+
+    def save(self):
+        """
+        Save the current state of the instance.
+        """
+        # Save method implementation
+        pass
+
+    def to_dict(self):
+        """
+        Returns a dictionary containing all keys/values of the instance.
+        """
+        # Conversion to dictionary
+        pass
