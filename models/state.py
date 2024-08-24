216c86fd33302b5ee4f2e15c4d7da078a8b57029
Muluken <mulukeneshetu70@gmail.com>
2024-08-24 18:48:38 +0300

    AirBnB_clone_v2

--- models/state.py
+++ models/state.py
@@ -6,16 +6,16 @@ in the AirBnB clone project.
 from models import storage
 from models.city import City
 
-class State:
-    """
-    The State class represents a state in the AirBnB clone.
-    """
 
+class State:
     def __init__(self, name):
         """Initializes a new State instance."""
         self.name = name
-        self.id = ... # Generate or assign a unique ID
+        self.id = None  # Use None instead of ...
 
     def cities(self):
         """Returns a list of cities related to this state."""
-        return [city for city in storage.all(City).values() if city.state_id == self.id]
+        return [
+            city for city in storage.all(City).values()
+            if city.state_id == self.id
+        ]
