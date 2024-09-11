f217c9c6117dca90190392a3c6ea0bac073f00f0
Muluken <mulukeneshetu70@gmail.com>
2024-09-11 12:27:24 +0300

    AirBnB_clone_v2

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,11 +1,68 @@
 #!/usr/bin/python3
-"""This module defines a class to manage DB storage for hbnb clone"""
+
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
-# other imports ...
+from models.base_model import Base
+from models.state import State
+import os
+
 
 class DBStorage:
-    # other methods ...
+    """Database storage class for interacting with the MySQL database."""
+    __engine = None
+    __session = None
+
+    def __init__(self):
+        """Initialize engine and drop all tables if environment is 'test'."""
+        user = os.getenv('HBNB_MYSQL_USER')
+        password = os.getenv('HBNB_MYSQL_PWD')
+        host = os.getenv('HBNB_MYSQL_HOST')
+        database = os.getenv('HBNB_MYSQL_DB')
+        env = os.getenv('HBNB_ENV')
+
+        # Create engine for the database
+        self.__engine = create_engine(
+            f'mysql+mysqldb://{user}:{password}@{host}/{database}',
+            pool_pre_ping=True
+        )
+
+        # Drop all tables if environment is 'test'
+        if env == 'test':
+            Base.metadata.drop_all(self.__engine)
+
+    def all(self, cls=None):
+        """Query all objects from the database.
+        If cls is specified, query only objects of that class.
+        """
+        if cls:
+            objs = self.__session.query(cls).all()
+        else:
+            objs = self.__session.query(State).all()
+        return {
+            f"{obj.__class__.__name__}.{obj.id}": obj for obj in objs
+        }
+
+    def new(self, obj):
+        """Add the object to the current database session."""
+        self.__session.add(obj)
+
+    def save(self):
+        """Commit all changes to the current database session."""
+        self.__session.commit()
+
+    def delete(self, obj=None):
+        """Delete the object from the current database session."""
+        if obj:
+            self.__session.delete(obj)
+
+    def reload(self):
+        """Create all tables in the database and initialize a new session."""
+        Base.metadata.create_all(self.__engine)
+        session_factory = sessionmaker(
+            bind=self.__engine, expire_on_commit=False
+        )
+        self.__session = scoped_session(session_factory)()
 
     def close(self):
-        """Close the current SQLAlchemy session"""
-        self.__session.remove()
+        """Close the current session."""
+        self.__session.close()
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,10 +1,56 @@
 #!/usr/bin/python3
-"""This module defines a class to manage file storage for hbnb clone"""
-import json
+"""Module for file storage."""
+
+import os
+import pickle
+
 
 class FileStorage:
-    # other methods ...
+    """Class for managing file storage."""
+
+    __file_path = 'file.json'
+    __objects = {}
+
+    def all(self, cls=None):
+        """Return a dictionary of objects or all objects of specific class."""
+        if cls is None:
+            return self.__objects
+
+        cls_name = cls.__name__
+        return {
+            key: value
+            for key, value in self.__objects.items()
+            if key.startswith(cls_name)
+        }
+
+    def new(self, obj):
+        """Add a new object to the storage."""
+        if obj is None:
+            return
+
+        key = f"{obj.__class__.__name__}.{obj.id}"
+        self.__objects[key] = obj
+
+    def save(self):
+        """Save the objects to a file."""
+        with open(self.__file_path, 'wb') as file:
+            pickle.dump(self.__objects, file)
+
+    def reload(self):
+        """Load objects from a file."""
+        if os.path.exists(self.__file_path):
+            with open(self.__file_path, 'rb') as file:
+                self.__objects = pickle.load(file)
+
+    def delete(self, obj=None):
+        """Delete an object from the storage."""
+        if obj is None:
+            return
+
+        key = f"{obj.__class__.__name__}.{obj.id}"
+        if key in self.__objects:
+            del self.__objects[key]
 
     def close(self):
-        """Call the reload method for deserializing the JSON file to objects"""
+        """Call the reload method to ensure objects are reloaded."""
         self.reload()
